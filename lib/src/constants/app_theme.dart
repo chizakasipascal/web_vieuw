1d6a4685d0da76661c2df71d40d69bd92f772a4b
Chiza Kasi Pascal <pascalkasichiza@gmail.com>
2020-10-21 05:49:03 -0700

    create widget  Buttom bar

---
+++ lib/src/constants/app_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:inoselsweb/utils/colors.dart';
+
+final ThemeData themeData = ThemeData(
+    backgroundColor: WhiteColor,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    primaryColor: InoselColor,
+    accentColor: InoselColor,
+    canvasColor: InoselColor);
+
+final TextTheme textTheme = TextTheme();
---
+++ lib/utils/colors.dart
@@ -0,0 +1,4 @@
+import 'dart:ui';
+
+const WhiteColor = const Color(0xFFFFFFFF);
+const InoselColor = const Color(0xFF3B163D);
