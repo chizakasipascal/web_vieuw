7b79f96359ec4b91547672cf14714e5f710d335f
Chiza Kasi Pascal <pascalkasichiza@gmail.com>
2020-10-29 03:41:27 -0700

    remove link not this for my class Links

--- lib/src/data/network/link.dart
+++ lib/src/data/network/link.dart
@@ -1,11 +1,5 @@
 class Links {
   Links._();
   // links
-
-  static const String link = "http://172.18.101.115:5400";
-  // static const String link = "https://inotel.be/";
-  // static const String link = "https://connectis.co/Login";
-  // static const String link = "https://dribbble.com/";
-  // static const String link = "https://google.com";
-  // static const String link = "http://github.com/";
+  static const String link = "https://dribbble.com/";
 }
